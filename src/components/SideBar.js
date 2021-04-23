0b960c045df56ac63cf38c1a8154549bf2bf2ed2
Kumud Shandilya <kumudshandilyag@gmail.com>
2021-04-23 16:22:59 +0530

    Change text color based on events

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -8,6 +8,13 @@ import "./SideBar.css";
 
 const SideBar = ({index, setIndex}) => {
 
+  const [classA, setClassA] = useState("lighter_name");
+  const [classa, setClassa] = useState("lighter_price");
+  const [classB, setClassB] = useState("darker_name");
+  const [classb, setClassb] = useState("darker_price");
+  const [classC, setClassC] = useState("darker_name");
+  const [classc, setClassc] = useState("darker_price");
+  const [addPrice, setAddPrice] = useState("+ FULL SELF DRIVING")
   const [display1, handleDisplay1] = useState(true);
   const [display2, handleDisplay2] = useState(false);
   const [description, setDescription] = useState("Fully refundable. You will be able to complete your configuration as production nears in late 2021. Single Motor RWD production is expected to begin in late 2022.");
@@ -19,19 +26,45 @@ const SideBar = ({index, setIndex}) => {
 
   const func_1 = (e) => {
     setIndex(0);
+    setClassA("lighter_name");
+    setClassa("lighter_price");
+    setClassB("darker_name");
+    setClassb("darker_price");
+    setClassC("darker_name");
+    setClassc("darker_price");
     setDescription("Fully refundable. You will be able to complete your configuration as production nears in late 2021. Single Motor RWD production is expected to begin in late 2022.");
   }
 
   const func_2 = (e) => {
     setIndex(1);
+    setClassB("lighter_name");
+    setClassb("lighter_price");
+    setClassA("darker_name");
+    setClassa("darker_price");
+    setClassC("darker_name");
+    setClassc("darker_price");
     setDescription("Fully refundable. You will be able to complete your configuration as production nears in late 2021. Dual Motor AWD production is expected in late 2021.");
   }
 
   const func_3 = (e) => {
     setIndex(2);
+    setClassC("lighter_name");
+    setClassc("lighter_price");
+    setClassB("darker_name");
+    setClassb("darker_price");
+    setClassA("darker_name");
+    setClassa("darker_price");
     setDescription("Fully refundable. You will be able to complete your configuration as production nears in late 2021. Tri Motor AWD production is expected in late 2021.");
   }
 
+  const func_4 = (e) => {
+    if(addPrice ==="+ FULL SELF DRIVING"){
+      setAddPrice("FULL SELF DRIVE ADDED");
+    }
+    else {
+      setAddPrice("+ FULL SELF DRIVING");
+    }
+  }
 
   return (
     <div class = "sidebar">
@@ -40,23 +73,23 @@ const SideBar = ({index, setIndex}) => {
         <Navbar className="navbar-dark bg-transparent nav">
 
               <Col xs = {12} className = "box" onClick = {func_1}>
-              <NavbarBrand><b className = "names">SINGLE MOTOR RWD</b></NavbarBrand>
-              <b className = "price">$39,000</b>
+              <NavbarBrand><b className = {classA}>SINGLE MOTOR RWD</b></NavbarBrand>
+              <b className = {classa}>$39,000</b>
               </Col>
               <Col xs = {12} className = "box" onClick = {func_2}>
-              <NavbarBrand><b className = "names">DUAL MOTOR AWD</b></NavbarBrand>
-              <b className = "price">$49,000</b>
+              <NavbarBrand><b className = {classB}>DUAL MOTOR AWD</b></NavbarBrand>
+              <b className = {classb}>$49,000</b>
               </Col>
               <Col xs = {12} className = "box" onClick = {func_3}>
-              <NavbarBrand><b className = "names">TRI MOTOR AWD</b></NavbarBrand>
-              <b className = "price">$69,000</b>
+              <NavbarBrand><b className = {classC}>TRI MOTOR AWD</b></NavbarBrand>
+              <b className = {classc}>$69,000</b>
               </Col>
         </Navbar>
         </Row>
         <Container>
         <Row className = "a">
         <Col xs = {12} className = "box">
-        <b className = "self_d">FULL SELF DRIVING</b>
+        <b className = "self_d" onClick = {func_4}>{addPrice}</b>
         <b className = "price_d">$19,000</b>
         </Col>
         <Col xs = {12}>
